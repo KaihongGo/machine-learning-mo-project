8e1a1e60a3edb79aefbbf6d3e3e926f92624e22f
Kaihong Yan <34575284+KaihongGo@users.noreply.github.com>
2020-11-22 15:18:24 +0800

    default network

--- Project 1/main.py
+++ Project 1/main.py
@@ -1,7 +1,8 @@
 # %%
 import copy
+import datetime
 
-import cv2  # oepnCV
+import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
@@ -10,7 +11,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 import torchvision.transforms as T
 from PIL import Image
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, dataset
 from torchvision.datasets import ImageFolder
 from tqdm.auto import tqdm  # 进度条
 
@@ -27,7 +28,7 @@ from torch_py.Utils import plot_image
 # 形成数据集，测试集DataLoader
 
 
-def processing_data(data_path, height=224, width=224, batch_size=32,
+def processing_data(data_path, height=224, width=224, batch_size=20,
                     test_split=0.1):
     """
     数据处理部分
@@ -69,9 +70,7 @@ train_data_loader, valid_data_loader = processing_data(
 
 # %%
 # 2.如果有预训练模型，则加载预训练模型；如果没有则不需要加载
-pnet_path = "./torch_py/MTCNN/weights/pnet.npy"
-rnet_path = "./torch_py/MTCNN/weights/rnet.npy"
-onet_path = "./torch_py/MTCNN/weights/onet.npy"
+
 
 # %%
 # 3.创建模型和训练模型，训练模型时尽量将模型保存在 results 文件夹
@@ -162,59 +161,110 @@ print('加载完成...')
 epochs = 20
 best_loss = 1e9
 best_model_weights = copy.deepcopy(model.state_dict())
-loss_list = []  # 存储损失函数值
+train_loss_list = []  # 存储损失函数值
+train_accuracy_list = []
+test_accuracy_list = []
+
+start_time = datetime.datetime.now()
 for epoch in range(epochs):
     model.train()
+    epoch_loss = []
+    epoch_train_accuracy = []
+    epoch_test_accuracy = []
 
-    for batch_idx, data in tqdm(enumerate(train_data_loader, 1)):
+    for batch_idx, data in enumerate(train_data_loader, 1):
         # get the inputs, data is a list of [inputs, labels]
         x, y = data
         x = x.to(device)
         y = y.to(device)
-
         # zero the parameter gradients
         optimizer.zero_grad()
-        # print(pred_y.shape)
-        # print(y.shape)
         # forward + backward + optimize
-        pred_y = model(x)
-        loss = criterion(pred_y, y)
+        output_y = model(x)
+        loss = criterion(output_y, y)
         loss.backward()
         optimizer.step()
-        # 记录损失
-        loss_list.append(loss)
-
         if loss < best_loss:
             best_model_weights = copy.deepcopy(model.state_dict())
             best_loss = loss
 
+        with torch.no_grad():
+            # 记录损失
+            train_loss_list.append(loss.item())
+            epoch_loss.append(loss.item())
+            # train accuracy
+            predict_y = torch.max(output_y, dim=1)[1]
+            accuracy = (predict_y == y).sum().item() / y.size(0)
+            train_accuracy_list.append(accuracy)
+            epoch_train_accuracy.append(accuracy)
+            # test accuracy
+            total = 0
+            correct = 0
+            for data in valid_data_loader:
+                images, labels = data
+                images = images.to(device)
+                labels = labels.to(device)
+                outputs = model(images)
+                predicted = torch.max(outputs.data, 1)[1]
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+            test_accuracy = correct / total
+            test_accuracy_list.append(test_accuracy)
+            epoch_test_accuracy.append(test_accuracy)
+
+    epoch_loss = np.mean(epoch_loss)
+    epoch_train_accuracy = np.mean(epoch_train_accuracy)
+    epoch_test_accuracy = np.mean(epoch_test_accuracy)
     print('step:' + str(epoch + 1) + '/' +
-          str(epochs) + ' || Total Loss: %.4f' % (loss.item()))
+          str(epochs) + ' || Total Loss: %.4f' % (epoch_loss) +
+          ' || Train accuracy: % .4f' % (epoch_train_accuracy) +
+          ' || Train accuracy: % .4f' % (epoch_test_accuracy))
 
 SAVE_PATH = 'results/temp.pth'
 torch.save(model.state_dict(), SAVE_PATH)
 print('Finish Training.')
 
-# loss
-plt.plot(loss_list, label="loss")
+end_time = datetime.datetime.now()
+print((end_time - start_time).seconds, "seconds")
+
+# %% 画图
+# train loss
+plt.plot(train_loss_list, label="train loss")
+plt.title('Loss')
 plt.legend()
+plt.xlabel('Batches')
+plt.ylabel('Loss')
 plt.show()
 
-
-# %%
-img = Image.open("test1.jpg")
-detector = FaceDetector()
-recognize = Recognition(model_path='results/temp.pth')
-draw, all_num, mask_nums = recognize.mask_recognize(img)
-plt.imshow(draw)
+plt.plot(train_accuracy_list, 'y',  label="train accuracy")
+plt.plot(test_accuracy_list, 'g', label="test accuracy")
+plt.legend()
+plt.title('Train and Test Accuracy')
+plt.xlabel('Batches')
+plt.ylabel('Accuracy')
 plt.show()
-print("all_num:", all_num, "mask_num", mask_nums)
 
 # %%
 # 4.评估模型，将自己认为最佳模型保存在 result 文件夹，其余模型备份在项目中其它文件夹，方便您加快测试通过。
 
-
 model_path = 'results/temp.pth'
+model = MobileNetV1().to(device)
+model.load_state_dict(torch.load(model_path))
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in valid_data_loader:
+        images, labels = data
+        images = images.to(device)
+        labels = labels.to(device)
+        outputs = model(images)
+        predicted = torch.max(outputs.data, 1)[1]
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+print('Accuracy of the network on the test images: %.2f %%' % (
+    100 * correct / total))
+# %%
+
 # ---------------------------------------------------------------------------
 
 
@@ -246,3 +296,11 @@ all_num, mask_num = predict(img)
 print(all_num, mask_num)
 
 # %%
+
+img = Image.open("test1.jpg")
+detector = FaceDetector()
+recognize = Recognition(model_path='results/temp.pth')
+draw, all_num, mask_nums = recognize.mask_recognize(img)
+plt.imshow(draw)
+plt.show()
+print("all_num:", all_num, "mask_num", mask_nums)
